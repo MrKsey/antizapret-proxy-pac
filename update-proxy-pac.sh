a19644fc643f63440d9dc7495604049d2c85e7a2
ksey <17622530+MrKsey@users.noreply.github.com>
2022-04-26 21:52:41 +0300

    Update update-proxy-pac.sh

--- update-proxy-pac.sh
+++ update-proxy-pac.sh
@@ -6,10 +6,4 @@ export UA="Mozilla/5.0 (X11; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0"
 # For local proxy (i.e. shadowsocks client for windows)
 export LOCAL_PROXY=127.0.0.1:1080
 
-# For remote proxy
-export REMOTE_PROXY=proxy-pac.home.arpa:1080
-# add in your system's or home router hosts file (/etc/hosts) an entry that specifies the hostname + IP address, where proxy server reside:
-# 192.168.1.10     proxy-pac.home.arpa
-
 curl -A "$UA" -L "$PROXY_PAC_URL" | sed -E "s/return \".+DIRECT\"/return \"PROXY $LOCAL_PROXY; DIRECT\"/" > ./local.pac
-cat ./local.pac | sed -E "s/return \"PROXY $LOCAL_PROXY; DIRECT\"/return \"PROXY $REMOTE_PROXY; SOCKS5 $REMOTE_PROXY; DIRECT\"/" > ./remote.pac

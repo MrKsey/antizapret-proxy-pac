ec88484157c4bd93eb33a9beda953a10389496fa
ksey <17622530+MrKsey@users.noreply.github.com>
2022-04-26 16:46:26 +0300

    Update update-proxy-pac.sh

--- update-proxy-pac.sh
+++ update-proxy-pac.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 export PROXY_PAC_URL="https://antizapret.prostovpn.org/proxy.pac"
+export UA="Mozilla/5.0 (X11; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0"
 export PROXY_IP_PORT=127.0.0.1:1080
 
-curl -A "Mozilla/5.0 (X11; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0" -L "https://antizapret.prostovpn.org/proxy.pac" | \
+curl -A "$UA" -L "$PROXY_PAC_URL" | \
 sed -E "s/return \".+DIRECT\"/return \"PROXY $PROXY_IP_PORT; DIRECT\"/" > ./proxy.pac
